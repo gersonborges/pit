420e738bc8d78650efd22e429f68d09188799825
Gerson Borges <passupreto@gmail.com>
2020-11-24 08:00:24 -0300

    Create senha.php

---
+++ session/senha.php
@@ -0,0 +1,12 @@
+<form action="./?classe=Usuario&acao=senha" method="post">
+    <div class="form-group">
+        <label for="email">Email:</label>
+        <input type="email" class="form-control" id="email" placeholder="Digite o email"
+               name="email" autofocus>
+    </div>
+
+    <p>Nós iremos enviar um email com instruções para você criar a sua nova senha.</p>
+
+    <button type="submit" class="btn btn-primary">Criar nova senha</button>
+
+</form>
